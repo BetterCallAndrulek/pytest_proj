dc0573f551390b2b54c14b7cabcd144e4775aa0c
BetterCallAndrulek <ddemon245@gmail.com>
2024-01-28 14:37:28 +0700

    исправления

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,10 +2,18 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2  # 3
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -1, "test") == "test"  # new
+    assert arrs.get([1, 2, 3], 4, "test") == "test"  # new
+    assert arrs.get([1, 2, 3], 3, "test") == "test"  # new
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -3, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -3, 2) == [2]
+    assert arrs.my_slice([1, 2, 3, 4], 3, 2) == []
+    assert arrs.my_slice([], 3, 2) == []
+    assert arrs.my_slice([1, 2, 3, 4], -5, 2) == [1, 2]
